d432898a0e41d1618f5972f51aa082d67b645459
rudrankbasant <rudrank123@gmail.com>
2023-08-16 15:52:31 +0530

    feat: otp edittext listeners

--- app/src/main/java/com/example/mefit/Verify.kt
+++ app/src/main/java/com/example/mefit/Verify.kt
@@ -2,16 +2,13 @@ package com.example.mefit
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mefit.databinding.ActivityMainBinding
 import com.example.mefit.databinding.ActivityVerifyBinding
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthProvider
 import com.google.firebase.firestore.ktx.firestore
@@ -33,6 +30,12 @@ class Verify : AppCompatActivity() {
         val etC5 = binding.etC5
         val etC6 = binding.etC6
 
+
+        val editTexts: List<EditText> by lazy {
+            listOf(etC1, etC2, etC3, etC4, etC5, etC6)
+        }
+
+        setupEditTextListeners(editTexts)
 
         verificationId = intent.getStringExtra("vfId")
         editTextInput()
@@ -103,6 +106,31 @@ class Verify : AppCompatActivity() {
         })
     }
 
+
+    private fun setupEditTextListeners(editTexts: List<EditText>) {
+        for (i in 0 until editTexts.size - 1) {
+            editTexts[i].addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    if (s?.length == 1) {
+                        editTexts[i + 1].requestFocus()
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                }
+            })
+        }
+
+        // Set last EditText's listener to hide the keyboard
+        editTexts[5].setOnEditorActionListener { _, _, _ ->
+            editTexts[5].clearFocus()
+            // Optionally, you can hide the keyboard here
+            true
+        }
+    }
     private fun editTextInput() {
         // Same logic here... you might want to consider refactoring this for brevity
         // ... (rest of your editTextInput method)
